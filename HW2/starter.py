ca654a8948fa3197c57022edca6ad32cc9f47e05
ascharie <102168338+ascharie@users.noreply.github.com>
2024-10-27 11:01:03 -0500

    first changes

--- HW2/starter.py
+++ HW2/starter.py
@@ -1,17 +1,77 @@
+import math
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
 # returns Euclidean distance between vectors and b
 def euclidean(a,b):
-    
+    assert(len(a) == len(b))
+    dist = 0
+    for i in range(len(a)):
+        dist += (b[i] - a[i])**2
+    dist = dist**0.5
+    assert(dist == np.linalg.norm(np.array(a)-np.array(b)))
     return(dist)
         
 # returns Cosine Similarity between vectors and b
 def cosim(a,b):
-    
+    dist = dot(a,b) / (euclidean(a,[0]*len(a)) * euclidean(b,[0]*len(b)))
     return(dist)
+
+# returns dot product of a and b
+def dot(a,b):
+    assert(len(a) == len(b))
+    dot = sum([a[i]*b[i] for i in range(len(a))])
+    assert(dot == np.dot(np.array(a), np.array(b)))
+    return(dot)
+
+# returns Pearson correlation coefficient between a and b
+def pearson(a,b):
+    assert(len(a) == len(b))
+    mean_a = sum(a) / len(a)
+    mean_b = sum(b) / len(b)
+    num = sum([(a[i] - mean_a) * (b[i] - mean_b) for i in range(len(a))])
+    denum = euclidean(a, [mean_a]*len(a)) * euclidean(b, [mean_b]*len(b))
+    p_coeff = num / denum
+    assert(p_coeff == np.corrcoef(np.array(a), np.array(b))[0][1])
+    return(p_coeff)
+
+# returns hamming distance between a and b
+# a and b are binary vectors
+def hamming(a,b):
+    assert(len(a) == len(b))
+    dist = [a[i] + b[i] for i in range(len(a))].count(1)
+    assert(dist == np.count_nonzero(np.array(a) - np.array(b)))
+    return dist
+
+# returns binary reduced data
+def binarize(X):
+    X_reduced = []
+    for i in X:
+        X_reduced.append([i[0], [0 if j == '0' else 1 for j in i[1]]])
+    return X_reduced
+
+# returns PCA reduced data
+def pca(X):
+    labels = [example[0] for example in X]
+    X_data = np.array([example[1] for example in X])
+
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X_data) # standardize data
+
+    pca = PCA(n_components=0.95)
+    X_pca = pca.fit_transform(X_scaled) # reduce dimensions
+
+    X_reduced = [[labels[i], list(X_pca[i])] for i in range(len(X_pca))]
+    return X_reduced
 
 # returns a list of labels for the query dataset based upon labeled observations in the train dataset.
 # metric is a string specifying either "euclidean" or "cosim".  
 # All hyper-parameters should be hard-coded in the algorithm.
 def knn(train,query,metric):
+
+
+
     return(labels)
 
 # returns a list of labels for the query dataset based upon observations in the train dataset. 
